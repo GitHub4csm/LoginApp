7f7adcf351b77976a642b3b54519fc36cc6a2721
cbp3hmm <chandrasaimohan.bhupathi@colruytgroup.com>
2022-11-04 23:30:21 +0100

    First Commit

---
+++ app/src/main/java/com/compose/loginapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.compose.loginapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.compose.loginapp.login.UserLoginScreen
+import com.compose.loginapp.ui.theme.LoginAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginAppTheme { // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+                ) {
+                   // Greeting("Android")
+                    UserLoginScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginAppTheme {
+        //Greeting("Android")
+         UserLoginScreen()
+    }
+}
---
+++ app/src/main/java/com/compose/loginapp/login/UserLogin.kt
@@ -0,0 +1,134 @@
+package com.compose.loginapp.login
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import com.compose.loginapp.R
+import com.compose.loginapp.ui.widgets.BasicTextField
+import com.jetpack.textfielddemo.ui.widgets.textField.PassWordTextField
+
+@Composable
+fun UserLoginScreen() {
+    var email  by rememberSaveable { mutableStateOf("") }
+    var password    by rememberSaveable { mutableStateOf("") }
+
+    Surface(
+        color = Color.LightGray, modifier = Modifier.fillMaxSize()
+    ) {
+        ConstraintLayout(
+            constraintSet = SetUserLoginConstraints(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)) {
+            val focusManager = LocalFocusManager.current
+            Image(
+                painter = painterResource(id = R.drawable.cart),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(48.dp)
+                    .layoutId("loginIcon")
+                    .clip(CircleShape),
+            )
+
+            BasicTextField(
+                text = email, onValueChange = { email = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .layoutId("userEmail"), label = { Text("Email") },
+                placeHolderText = { Text("Enter Email") }, imeAction = ImeAction.Next,
+                keyboardType = KeyboardType.Email, keyboardActions = KeyboardActions(onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                })
+            )
+            PassWordTextField(text = password, onValueChange = { password = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .layoutId("userPassword"),
+                label = { Text("Password") }, placeHolderText = { Text("Enter Password") },
+                imeAction = ImeAction.Next, keyboardType = KeyboardType.Password,
+                keyboardActions = KeyboardActions(onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                })
+            )
+
+            Button(
+                onClick = { },
+                modifier = Modifier.layoutId("loginBtn"),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue, contentColor = Color.White)
+            ) {
+                Text(text = "Login",
+                     fontSize = 12.sp,
+                     fontWeight = FontWeight.Medium
+
+                   )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun SetUserLoginConstraints():ConstraintSet {
+    return ConstraintSet {
+        val loginImageConstraint = createRefFor("loginIcon")
+        val loginUserEmailConstraint = createRefFor("userEmail")
+        val loginPasswordConstraint = createRefFor("userPassword")
+        val loginBtnConstraint = createRefFor("loginBtn")
+
+        constrain(loginImageConstraint){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            bottom.linkTo(parent.bottom)
+        }
+        constrain(loginUserEmailConstraint){
+            top.linkTo(loginImageConstraint.bottom, margin=10.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+        constrain(loginPasswordConstraint){
+            top.linkTo(loginUserEmailConstraint.bottom, margin=10.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(loginBtnConstraint){
+            top.linkTo(loginPasswordConstraint.bottom, margin=10.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+    }
+}
